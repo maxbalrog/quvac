3a578d3ca420d72d8cf04252aec430a004fc3fd9
maxbalrog <maxbalrog@gmail.com>
2024-09-06 09:33:39 +0200

    Draft template for ParaxialGaussian

--- src/quvac/paraxial_gaussian.py
+++ src/quvac/paraxial_gaussian.py
@@ -2,10 +2,13 @@
 '''
 TODO:
     - Add units to variables description
+    - Test how transformed coordinates look like?
+    - Check 0-order paraxial with plotting
 '''
 
 import numpy as np
 from scipy.constants import pi, c, epsilon_0
+from scipy.spatial.transform import Rotation
 
 
 class ParaxialGaussianAnalytic(object):
@@ -40,11 +43,16 @@ class ParaxialGaussianAnalytic(object):
     def __init__(self, field_params, grid):
         # Dynamically create class instance variables available with 
         # self.<variable_name>
+        angles = 'theta phi beta phase0'.split()
         for key,val in field_params.items():
-            self.__dict__[key] = val
+            if key in angles:
+                self.__dict__[key] = val * np.pi / 180
+            else:
+                self.__dict__[key] = val
 
         # Define grid variables
-        self.x, self.y, self.z = grid
+        self.grid = grid
+        self.x_, self.y_, self.z_ = grid
         self.grid_shape = [dim.size for dim in grid]
 
         # Define additional field variables
@@ -52,4 +60,38 @@ class ParaxialGaussianAnalytic(object):
         self.k = 2. * pi / self.lmbd
         self.omega = c * self.k
         self.zR = pi * self.w0**2 / self.lmbd
+
+        # Define rotation matrices and transform coordinate grid
+        self.get_rotation()
+        self.transform_coordinates()
+
+        # Define variables not depending on time step
+        self.w = self.w0 * np.sqrt(1 + (self.z/self.zR)**2)
+        self.r2 = self.x**2 + self.y**2
+        self.R = self.z + self.zR**2/self.z
+        self.phase_no_t = self.phase0 - self.k*self.r2/(2*self.R) + np.arctan(self.z/self.zR)
+        self.E = self.E0 * self.w0/self.w * np.exp(-self.r2/self.w**2)
+
+
+    def get_rotation(self):
+        # Define rotation transforming (0,0,1) -> (kx,ky,kz) for vectors
+        self.rotation = Rotation.from_euler('zyz', (self.phi,self.theta,self.beta))
+        self.rotation_m = self.rotation.as_matrix()
+        # Inverse rotation: (kx,ky,kz) -> (0,0,1)
+        self.rotation_bwd = self.rotation.inv()
+        self.rotation_bwd_m = self.rotation_bwd.as_matrix()
+
+    def transform_coordinates(self):
+        axes = 'xyz'
+        for i,ax in enumerate(axes):
+            mx, my, mz = self.rotation_bwd_m[i]
+            self.__dict__[ax] = mx*(self.x_-self.x0) + my*(self.y_-self.y0) + mz*(self.z_-self.z0)
+    
+    def calculate_field(self, t):
+        psi_plane = self.w*(t-self.t0) - self.k*self.z
+        phase = self.psi_no_t + psi_plane
+        Ex = self.E * np.exp(-(psi_plane/self.omega)**2/(self.tau/2)**2) * np.cos(phase)
+        return Ex
+        
+
 
