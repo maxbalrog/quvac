8a8a89528a178e361ff1a60cbb89e635a3466a3c
maxbalrog <maxbalrog@gmail.com>
2024-11-11 17:16:20 +0100

    ref (tests): combine several test files into one and use default field and grid configuration

--- tests/test_simulation_script.py
+++ tests/test_simulation_script.py
@@ -1,86 +1,26 @@
 '''
 Here we provide a test for quantum vacuum signal calculator
-with script
+with script in several scenarios:
+1) test_simulation: default collision scenario
+2) test_mixed_fields: one field is analytic, another is maxwell
+3) test_spherical: interpolation of signal spectrum on spherical grid
 '''
 
 import os
 from pathlib import Path
 
+import pytest
 import numpy as np
-from scipy.constants import c
 
-from quvac.grid import get_xyz_size, get_t_size
-from quvac.utils import write_yaml
+from quvac.analytic_scalings import get_two_paraxial_scaling
+from quvac.utils import read_yaml, write_yaml
+from config import DEFAULT_CONFIG_PATH
 
 
 SCRIPT_PATH = 'src/quvac/simulation.py'
 
 
-def test_simulation():
-    # Define field parameters
-    tau = 25e-15
-    W = 25
-    lam = 0.8e-6
-    w0 = 2*lam
-    theta = 90
-    beta = 90
-
-    # Define fields
-    field_1 = {
-        "field_type": "paraxial_gaussian_analytic",
-        "focus_x": [0.,0.,0.],
-        "focus_t": 0.,
-        "theta": 0,
-        "phi": 0,
-        "beta": 0,
-        "lam": lam,
-        "w0": w0,
-        "tau": tau,
-        "W": W,
-        "phase0": 0,
-    }
-
-    field_2 = {
-        "field_type": "paraxial_gaussian_analytic",
-        "focus_x": [0.,0.,0.],
-        "focus_t": 0.,
-        "theta": theta,
-        "phi": 0,
-        "beta": beta,
-        "lam": lam,
-        "w0": w0,
-        "tau": tau,
-        "W": W,
-        "phase0": 0,
-    }
-
-    fields_params = {
-        'field_1': field_1,
-        'field_2': field_2,
-    }
-
-    # Set up grid parameters
-    x0, y0, z0 = 5*c*tau, 12*w0, 5*c*tau
-    box_size = np.array([x0, y0, z0])/2
-    Nxyz = get_xyz_size(fields_params, box_size)
-    Nx, Ny, Nz = Nxyz
-
-    t0 = 4*tau
-    Nt = get_t_size(-t0/2, t0/2, lam)
-
-    ini_data = {
-        'fields': fields_params,
-        'grid': {
-            'mode': 'direct',
-            'box_xyz': [x0,y0,z0],
-            'Nxyz': [Nx,Ny,Nz],
-            'box_t': t0,
-            'Nt': Nt
-        },
-        'performance': {}
-    }
-
-    path = 'data/test/test_sim'
+def run_test_simulation(path, ini_data):
     Path(path).mkdir(parents=True, exist_ok=True)
 
     ini_file = os.path.join(path, 'ini.yml')
@@ -89,4 +29,125 @@ def test_simulation():
     # Launch simulation
     status = os.system(f"{SCRIPT_PATH} --input {ini_file}")
     assert status == 0, "Script execution did not finish successfully"
+
+
+def test_simulation():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+    
+    path = 'data/test/test_simulation'
+    run_test_simulation(path, ini_data)
+
+
+def test_compare_with_analytics():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+    field_1_params = {
+        'field_type': 'paraxial_gaussian_analytic',
+        'w0': 2*0.8e-6,
+    }
+    field_2_params = {
+        'field_type': 'paraxial_gaussian_analytic',
+        'w0': 2*0.8e-6,
+        'theta': 90,
+        'beta': 90
+    }
+    ini_data['fields']['field_1'].update(field_1_params)
+    ini_data['fields']['field_2'].update(field_2_params)
+    ini_data['grid']['collision_geometry'] = 'xz'
+    
+    path = 'data/test/test_compare_with_analytics'
+    run_test_simulation(path, ini_data)
+
+    data = np.load(os.path.join(path, 'spectra.npz'))
+    N_signal_num = data['N_total']
+
+    fields = list(ini_data['fields'].values())
+    N_signal_th, N_perp_th = get_two_paraxial_scaling(fields)
+
+    err_msg = "Analytical and numerical total signal differ by more than 1%"
+    assert np.isclose(N_signal_th, N_signal_num, rtol=1e-2), err_msg
+
+
+def test_mixed_fields():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+    ini_data['fields']['field_1']['field_type'] = "paraxial_gaussian_analytic"
+
+    path = 'data/test/test_mixed_fields'
+    run_test_simulation(path, ini_data)
+
+
+def test_spherical_grid():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+    ini_data['postprocess']['calculate_spherical'] = True
+    ini_data['postprocess']['calculate_discernible'] = False
+
+    path = 'data/test/test_spherical_grid'
+    run_test_simulation(path, ini_data)
+
+    data = np.load(os.path.join(path, 'spectra.npz'))
+    err_msg = 'Total signal on cartesian and spherical grid differ by more than 1%'
+    assert np.isclose(data['N_total'], data['N_sph_total'], rtol=1e-2), err_msg
+
+
+@pytest.mark.slow
+def test_discernible():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+    # Change field parameters
+    field_2_params = {
+        'w0': 4*0.8e-6,
+        'theta': 160,
+        'beta': 90
+    }
+    ini_data['fields']['field_2'].update(field_2_params)
+    ini_data['postprocess']['calculate_spherical'] = True
+    ini_data['postprocess']['calculate_discernible'] = True
+
+    path = 'data/test/test_discernible'
+    run_test_simulation(path, ini_data)
+
+    data = np.load(os.path.join(path, 'spectra.npz'))
+    N_disc = data['N_disc']
+    N_disc_expected = 2.5
+    err_msg = 'Calculated discernible signal differs from expected by more than 20%'
+    assert np.isclose(N_disc, N_disc_expected, rtol=2e-1), err_msg
+
+
+def test_channels():
+    # Load default simulation parameters
+    ini_data = read_yaml(DEFAULT_CONFIG_PATH)
+
+    results = []
+    for idx,channels in zip([(0,0), (0,1), (1,0)], [False, True, True]):
+        integrator_type = 'vacuum_emission_channels' if channels else 'vacuum_emission'
+
+        ini_data['integrator'] = {
+            'type': integrator_type,
+            'probe_pump_idx': {
+                'probe': [idx[0]],
+                'pump': [idx[1]],
+            }
+        }
+
+        path = f'data/test/test_channels_{idx[0]}_{idx[1]}'
+        run_test_simulation(path, ini_data)
+
+        data_file = os.path.join(path, 'spectra.npz')
+        data = np.load(data_file)
+        
+        N_signal_num = data['N_total']
+        results.append(N_signal_num)
+    
+    # Test that signal_channel_1 + signal_channel_2 = total_signal
+    # and signal_channel_1 == signal_channel_2
+    err_msg = 'Signal is different in two channels'
+    assert np.isclose(results[1], results[2], rtol=1e-5), err_msg
+    err_msg = 'Signal in two channels does not add up to the total signal'
+    assert np.isclose(results[0], results[1] + results[2], rtol=1e-2), err_msg
+
+
+
 
